38eca017924fa6a76d2f7eef00e54461b515b295
CodeInBars <ismaclaseifc3@gmail.com>
2019-05-10 10:44:31 +0200

    Fifth Commit-Modified Lines

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,10 @@
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Line 1");
-		System.out.println("Line 2");
+		System.out.println("Line 1 modified");
+		//System.out.println("Line 2");
 		System.out.println("Line 3");
+		System.out.println("Line 4");
 	}
 
 }
